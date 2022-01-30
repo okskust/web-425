deab274d715114fe457d51de1e582cb8c53ca590
Oksana Kustova <q100wa@gmail.com>
2022-01-30 14:51:39 -0500

    Added assignment 4.4.

---
+++ week-4/enterprise-composer-app/src/app/app.module.ts
@@ -0,0 +1,38 @@
+/**
+ * Title: app.module.ts
+ * Author: Oksana Kustova
+ * Date: 1/16/2022
+ * Description: The root module, which is responsible for the entire application.
+ */
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComposerListComponent } from './composer-list/composer-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ComposerDetailsComponent } from './composer-details/composer-details.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComposerListComponent,
+    ContactComponent,
+    AboutComponent,
+    ComposerDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
