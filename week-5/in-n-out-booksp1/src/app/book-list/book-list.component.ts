e0d58fa505061b7e165110e0e38e0a26fa753539
Oksana Kustova <q100wa@gmail.com>
2022-02-03 18:47:12 -0500

    Added Exercise 5.2.

---
+++ week-5/in-n-out-booksp1/src/app/book-list/book-list.component.ts
@@ -0,0 +1,21 @@
+/**
+ * Title: book-list.components.ts
+ * Author: Oksana Kustova
+ * Date: 02/02/2022
+ * Description: BookListComponent.
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss']
+})
+export class BookListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
