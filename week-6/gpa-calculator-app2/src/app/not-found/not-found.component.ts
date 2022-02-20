afce2e03992d168637f79595bcb8bdcef8273acc
Oksana Kustova <q100wa@gmail.com>
2022-02-20 14:28:36 -0500

    Added 6.4.

---
+++ week-6/gpa-calculator-app2/src/app/not-found/not-found.component.ts
@@ -0,0 +1,23 @@
+/**
+ * Title: not-found.components.ts
+ * Author: Oksana Kustova
+ * Date: 02/13/2022
+ * Description: component.
+ */
+
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.scss']
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ week-6/gpa-calculator-app2/src/app/transcript.interface.ts
@@ -0,0 +1,11 @@
+/**
+ * Title: transcript.interface.ts
+ * Author: Oksana Kustova
+ * Date: 02/13/2022
+ * Description: interface object
+ */
+
+export interface ITranscript {
+  course: string;
+  grade: string;
+}
